a34967cd68622702183c0f0d6c7fdbff650e0b26
Yuvadi29 <adityatrivedi612@gmail.com>
2023-11-18 22:07:44 +0530

    Write a C Program to accept a number and display its square

--- Fundamentals/01-Fundamentals.c
+++ Fundamentals/01-Fundamentals.c
@@ -1,14 +1,14 @@
 // We are given a program to accept a number and display its square
 #include <stdio.h>
-#include <conio.h>
+#include <conio.h> // This header is used for console I/O, but it's non-standard. Consider using alternatives for portability.
 
 void main()
 {
     int num, square;
     printf("Enter a number: ");
-    scanf("%d", &num);
+    scanf("%d", &num); // Taking input from the user and storing it in the 'num' variable.
 
-    square = num * num;
-    
-    printf("Square of the number: %d", square);
+    square = num * num; // Calculating the square of the input number and storing it in the 'square' variable.
+
+    printf("Square of the number: %d", square); // Displaying the square of the entered number.
 }
