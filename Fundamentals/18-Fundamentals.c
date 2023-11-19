8ba5473c8aa66e73bbbd0de0156248ce0fb446fd
Yuvadi29 <adityatrivedi612@gmail.com>
2023-11-20 02:11:39 +0530

    Write a C program to accept a character and display its equivalent ASCII

---
+++ Fundamentals/18-Fundamentals.c
@@ -0,0 +1,11 @@
+// Program to display ASCII value of number
+#include <stdio.h>
+
+void main()
+{
+    char ch;
+    printf("Enter a Character: ");
+    scanf("%c", &ch);
+
+    printf("ASCII Equivalent: %d", ch);
+}
