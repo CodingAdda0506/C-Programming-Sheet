b1aa17b5337a48e226fea37543bb8017fbb9d274
Yuvadi29 <adityatrivedi612@gmail.com>
2023-11-30 21:33:25 +0530

    Find Output #7

---
+++ Fundamentals/26-Fundamentals.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int x = 1;
+    printf("%d%d%d\n", x, (x = x + 2), (x << 2));
+    x << 2;
+    printf("%d%d%d\n", ++x, x++, ++x);
+    getch();
+}
