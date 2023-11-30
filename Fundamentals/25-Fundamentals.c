05b74e47df24d32ad86edc575db638aabd167dad
Yuvadi29 <adityatrivedi612@gmail.com>
2023-11-30 21:32:41 +0530

    Find Output #6

---
+++ Fundamentals/25-Fundamentals.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int a = 10, b, c;
+    c = b = a;
+    b -= a--;
+    c -= --a;
+    a -= --a - a--;
+    printf("a=%d\nb=%d\nc=%d\n", a, b, c);
+    getch();
+}
