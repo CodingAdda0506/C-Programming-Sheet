ed7b6e39ecbf1ebec2f154f0f16c92f7b70112cc
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-01 16:51:50 +0530

    Write a program to display first 10 natural numbers

---
+++ ControlStructures-Loops/01-Loops.c
@@ -0,0 +1,13 @@
+// Write a program to display first ten natural numbers
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    printf("First 10 Natural Numbers are: \n");
+    // Iterate from 1 to 10 using for loop
+    for (int i = 1; i <= 10; i++)
+    {
+        // print each iteration
+        printf("%d\n", i);
+    }
+}
