e787ced6b21e7a2c072a923590f9887cb9b73545
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-25 17:33:09 +0530

    Find the Output #1 - Loops

---
+++ ControlStructures-Loops/14-Loops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int i, j;
+    for (int i = 0; i <= 5; i++)
+    {
+        for (int j = 0; j <= 2; j++)
+        {
+            printf("Hello ");
+        }
+        printf("Hi \n");
+    }
+    }
